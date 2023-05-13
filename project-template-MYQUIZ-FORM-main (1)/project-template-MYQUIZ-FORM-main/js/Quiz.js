2329d1abb2a5a55e0bb332699a7e000d9b2908cd
Ankita <123867343+AnkitaSavla-28@users.noreply.github.com>
2023-05-14 01:51:21 +0530

    Add files via upload

---
+++ project-template-MYQUIZ-FORM-main (1)/project-template-MYQUIZ-FORM-main/js/Quiz.js
@@ -0,0 +1,15 @@
+class Quiz {
+  constructor(){}
+
+  start(){
+    this.question("Question: What starts with Letter'E' but has only one letter? ");
+      question = new Question()
+      question.display();
+    
+  }
+   end(){
+    this.message("Thank You, Your Answer Has Been Submitted");
+    this.message.position(350,350);
+   }
+  
+}
